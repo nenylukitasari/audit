66f08010cfe17b5747fba50afa1575f235001f5f
nenylukitasari <nenylukitasari@gmail.com>
2019-03-14 14:09:38 +0700

    Update makan_lembur.blade.php

--- resources/views/makan_lembur.blade.php
+++ resources/views/makan_lembur.blade.php
@@ -14,15 +14,7 @@
    <!-- Form -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"/>
-  <!-- CSRF token for ajax call -->
-  {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
-  {{-- 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> --}}
-
-
-@endsection
+ @endsection
 @section('content')
 <br/>
       <div class="row">
@@ -33,8 +25,86 @@
               <button type="button" class="btn btn-default pull-right" data-toggle="modal" data-target="#addModal"><i class="fa fa-plus"></i>&nbsp;&nbsp;&nbsp;Tambah
           </button>
             </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>Uraian Kegiatan</th>
+                  <th>Satuan</th>
+                  <th>Besaran Bruto Maksimum (Rp)</th>
+                  <th>Aksi</th>
+                </tr>
+                </thead>
+               <tbody>
+                  @foreach ($kategori_makans as $kategori_makan)
+                    <tr>
+                      <th>
+                        {{ $kategori_makan->kategori_makan}}
+                      </th>
+                      <td></td>
+                      <td></td>
+                      <td> 
+                        <i class="fa fa-eye" data-toggle="modal" data-target="#show-modal"> | </i>
+                          <i class="fa fa-pencil" data-toggle="modal" data-target="#edit-modal"> </i>
+                        </td>
+                    </tr>
+                    <tr>
+                    @foreach ($kategori_makan->makan_lembur as $makan)
+                      <td>
+                          {{ $makan->uraian_kegiatan}}
+                      </td>
+                      
+                      <td>
+                          {{ $makan->satuan}}
+                      </td>
+                      @if($makan->bruto == null)
+                      <td>
+                      </td>
+                      @else
+                      <td>
+                      {{number_format($makan->bruto)}}</td>
+                      @endif 
+                      <td>
+                          <i class="fa fa-eye" data-toggle="modal" data-target="#show-modal"> | </i> 
+                          <i class="fa fa-pencil" data-toggle="modal" data-target="#edit-modal"> </i>
+                          </td>
+                    </tr>
+                      @foreach ($makan->biaya_konsumsi as $biaya)
+                        <td>
+                          {{ $biaya->uraian_kegiatan}}
+                        </td>
+                        
+                        <td>
+                            {{ $biaya->satuan}}
+                        </td>
+                        @if($biaya->bruto == null)
+                        <td>
+                        </td>
+                        @else
+                        <td>
+                          {{number_format($biaya->bruto)}}</td>
+                        @endif 
+                        <td>
+                          <i class="fa fa-eye" data-toggle="modal" data-target="#show-modal"> | </i>
+                          <i class="fa fa-pencil" data-toggle="modal" data-target="#edit-modal"> </i>
+                        </td>
+                        <tr>
+                      @endforeach
+                    @endforeach
+                  @endforeach 
+                </tfoot>
+              </table>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+      </div>
 
- {{-- <div class="modal fade" id="modal-default"> --}}
+  <!--Add Modal-->
         <div id="addModal" class="modal fade">
           <div class="modal-dialog">
             <div class="modal-content">
@@ -55,8 +125,6 @@
                           </ul>
                       </div>
                   @endif
-
-              
                 {{-- <div class="alert alert-danger print-error-msg" style="display:none">
                   <ul></ul>
                 </div>  --}}
@@ -69,7 +137,6 @@
                         @endforeach
                       </select>  
                     </div>
-
             <form class="form-horizontal">
               <div class="box-body">
                 <div class="form-group">
@@ -94,10 +161,8 @@
                 </div>
               </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>                
+            <div class="modal-footer">  
                 <input type="submit" name="submit" id="submit" class="btn btn-primary" value="Tambah" />  
-                {{-- <button class="btn btn-success btn-submit">Submit</button> --}}
               </div>
             </form>
           </div>
@@ -107,102 +172,118 @@
         </div>
         <!-- /.modal -->
 
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>Uraian Kegiatan</th>
-                  <th>Satuan</th>
-                  <th>Besaran Bruto Maksimum (Rp)</th>
-                  <th>Aksi</th>
-                </tr>
-                </thead>
-               <tbody>
-                  @foreach ($kategori_makans as $kategori_makan)
-                    <tr>
-                      <th>
-                        {{ $kategori_makan->kategori_makan}}
-                      </th>
-                      <td></td>
-                      <td></td>
-                      <td> 
-                        <i class="fa fa-eye" data-toggle="modal" data-target="#show-modal"> | 
-                          <i class="fa fa-pencil" data-toggle="modal" data-target="#edit-modal"> 
-                        {{-- <button type="button" class="btn btn-info" data-toggle="modal" data-target="#modal-edit">Edit --}}
-                          </button></td>
-                    </tr>
-                    <tr>
-                    @foreach ($kategori_makan->makan_lembur as $makan)
-                      <td>
-                          {{ $makan->uraian_kegiatan}}
-                      </td>
-                      
-                      <td>
-                          {{ $makan->satuan}}
-                      </td>
-                      @if($makan->bruto == null)
-                      <td>
-                      </td>
-                      @else
-                      <td>
-                      {{number_format($makan->bruto)}}</td>
-                      @endif 
-                      <td>
-                          <i class="fa fa-eye" data-toggle="modal" data-target="#show-modal"> | 
-                          <i class="fa fa-pencil" data-toggle="modal" data-target="#edit-modal"> 
-                          </td>
-                    </tr>
-                      @foreach ($makan->biaya_konsumsi as $biaya)
-                        <td>
-                          {{ $biaya->uraian_kegiatan}}
-                        </td>
-                        
-                        <td>
-                            {{ $biaya->satuan}}
-                        </td>
-                        @if($biaya->bruto == null)
-                        <td>
-                        </td>
-                        @else
-                        <td>
-                          {{number_format($biaya->bruto)}}</td>
-                        @endif 
-                        <td>
-                          <i class="fa fa-eye" data-toggle="modal" data-target="#show-modal"> | 
-                          <i class="fa fa-pencil" data-toggle="modal" data-target="#edit-modal"> </td>
-                        <tr>
-                      @endforeach
-                    @endforeach
-                  @endforeach 
-                </tfoot>
-              </table>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-      </div>
-
-      <!-- Edit Modal -->
-   <div class="modal modal-info fade" id="modal-edit">
+  <!-- Show Modal -->
+  <div class="modal fade" id="show-modal">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Info Modal</h4>
+                <h4 class="modal-title">Show Modal</h4>
               </div>
               <div class="modal-body">
-                <p>One fine body&hellip;</p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-outline">Save changes</button>
+              <div class="box-body">
+                <table border="0">
+                  <tr>
+                    <th class="col-sm-9 control-label">ID</th>
+                    <td style="width: 10px">:</td>
+                    <td>lalala</td>
+                  </tr>
+                  <tr>
+                    <th class="col-sm-9 control-label">Kategori</th>
+                    <td style="width: 10px">:</td>
+                    <td>wakkaka</td>
+                  </tr>
+                  <tr>
+                    <th class="col-sm-9 control-label">Uraian Kegiatan</th>
+                    <td style="width: 10px">:</td>
+                    <td>wakkaka</td>
+                  </tr>
+                  <tr>
+                    <th class="col-sm-9 control-label">Satuan</th>
+                    <td style="width: 10px">:</td>
+                    <td>wakkaka</td>
+                  </tr>
+                  <tr>
+                    <th class="col-sm-9 control-label">Bruto</th>
+                    <td style="width: 10px">:</td>
+                    <td>wakkaka</td>
+                  </tr>
+                </table>
+              </div>              
               </div>
             </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+      
+    </div>
+
+
+  <!-- Edit Modal -->
+  <div id="edit-modal" class="modal fade">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Edit Modal</h4>
+              </div>
+              <div class="modal-body">
+                <form action="{{url('/makan_lembur_rapat')}}" method="POST" name="add_makan" id="add_makan"> 
+              {{-- <form name="add_makan" id="add_makan">  --}}
+                  @if ($errors->any())
+                      <div class="alert alert-danger">
+                          <ul>
+                              @foreach ($errors->all() as $error)
+                                  <li>{{ $error }}</li>
+                              @endforeach
+                          </ul>
+                      </div>
+                  @endif
+                {{-- <div class="alert alert-danger print-error-msg" style="display:none">
+                  <ul></ul>
+                </div>  --}}
+                  {{csrf_field()}} 
+                    <div class="form-group">
+                      <select class="form-control select2" style="width:500px;" name="kategori_makan">
+                        <option></option>
+                        @foreach($kategoris as $kategori)
+                        <option value="{{$kategori->id}}">{{$kategori->kategori_makan}}</option>
+                        @endforeach
+                      </select>  
+                    </div>
+            <form class="form-horizontal">
+              <div class="box-body">
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Uraian</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="uraian_kegiatan" placeholder="Uraian Kegiatan" class="form-control" />
+                  </div>
+                </div>
+                <br/><br/>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Satuan</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="satuan" placeholder="Satuan" class="form-control" />
+                  </div>
+                </div>
+                <br/><br/>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Bruto</label>
+                  <div class="col-sm-10">
+                    <input type="number" name="bruto" placeholder="Besaran Bruto Maksimum (Rp)" class="form-control" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">  
+                <input type="submit" name="submit" id="submit" class="btn btn-primary" value="Tambah" />  
+              </div>
+            </form>
+          </div>
             <!-- /.modal-content -->
           </div>
           <!-- /.modal-dialog -->
@@ -236,7 +317,6 @@
 
 <!-- form -->
 <script src="{{url('assets/bower_components/select2/dist/js/select2.full.min.js')}}"></script>        
-
 
 <script>
   $(function () {
