68642208c7b053c2a1d4e7a23c2b71723382dba5
nenylukitasari <nenylukitasari@gmail.com>
2019-06-13 20:14:52 +0700

    sudah selesai insyaAllah

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -84,18 +84,11 @@ else {
                 <a class="logo" href="index.html">
                     <!-- Logo icon image, you can use font-icon also --><b>
                     <!--This is dark logo icon--><img src="{{url('assets/plugins/images/admin-logo.png')}}" alt="home" class="dark-logo" /><!--This is light logo icon--><img src="{{url('assets/plugins/images/admin-logo-dark.png')}}" alt="home" class="light-logo" />
-                 </b>
-                    <!-- Logo text image you can use text also --><span class="hidden-xs">
-                    <!--This is dark logo text--><img src="{{url('assets/plugins/images/admin-text.png')}}" alt="home" class="dark-logo" /><!--This is light logo text--><img src="{{url('assets/plugins/images/admin-text-dark.png')}}" alt="home" class="light-logo" />
-                 </span> </a>
+                 </b> </a>
             </div>
             <!-- /Logo -->
             <!-- /Search Bar -->
             <ul class="nav navbar-top-links navbar-right pull-right">
-            <li>
-                    <form role="search" class="app-search hidden-sm hidden-xs m-r-10">
-                        <input type="text" placeholder="Search..." class="form-control"> <a href=""><i class="fa fa-search"></i></a> </form>
-                </li>
                 <li class="dropdown">
                     <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#"> <img src="<?php echo $picture; ?>" alt="user-img" width="36" class="img-circle"><b class="hidden-xs"><?php echo $username; ?></b><span class="caret"></span> </a>
                     <ul class="dropdown-menu dropdown-user animated flipInY">
